a83213ab10138ba4dc2772e518baab346d26a5d9
Augusto <augustocardenas96@gmail.com>
2021-06-07 13:07:58 -0300

    Nuevo reloj q funciona
    hay q hacer q no este invertido

---
+++ Nuevo Reloj/string.js
@@ -0,0 +1,24 @@
+var hr = document.getElementById('hr');
+var min = document.getElementById('min');
+var sec = document.getElementById('sec');
+
+
+function initClock(){
+
+    var date = new Date();
+    var hour = date.getHours() % 12;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    var hourDeg = hour * 30;
+    var minuteDeg = minute * 6;
+    var secondDeg = second * 6;
+    
+    hr.style.transform = 'rotate(' + hourDeg + 'deg)';
+    min.style.transform = 'rotate(' + minuteDeg + 'deg)'; 
+    sec.style.transform = 'rotate(' + secondDeg + 'deg)'; 
+
+    setTimeout(initClock, 1000);
+};
+
+initClock();
